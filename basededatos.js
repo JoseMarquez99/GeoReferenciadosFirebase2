38d3dbcbe019f1a432b0694f60d631e6943ac4dc
JoseMarquez99 <jamr1999@hotmail.com>
2021-05-03 00:18:03 -0500

    NICE

---
+++ basededatos.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDvJvxNxQjLAVqirtld5b9WcmD4BaC1lPo",
+            authDomain: "proyecto1-3b4fb.firebaseapp.com",
+            projectId: "proyecto1-3b4fb",
+            storageBucket: "proyecto1-3b4fb.appspot.com",
+            messagingSenderId: "576861727328",
+            appId: "1:576861727328:web:401cb6793b3d538f443105"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
